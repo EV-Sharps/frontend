5e653419595bb14cf784e0c0b3ef49f7a0b0e4c5
Zack Hecht <zhecht7@gmail.com>
2025-11-14 14:08:57 -0500

    dvp

--- shared.js
+++ shared.js
@@ -2586,3 +2586,11 @@ function startHelpTour() {
 	});
 	tour.start();
 }
+
+function debounce(fn, delay = 400) {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn(...args), delay);
+  };
+}

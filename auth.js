5e919870163b8a507a7a42f0e6eb98403db9a66a
Zack Hecht <zhecht7@gmail.com>
2025-12-30 19:04:42 -0500

    free over

--- auth.js
+++ auth.js
@@ -406,7 +406,7 @@ async function handleSession() {
 				fetchTuddys();
 			}
 		}, 30 * 1000);
-	} else if (["mlb", "outliers", "nhl", "atgs", "atgs2", "nba", "pts", "analysis", "ncaab"].includes(PAGE)) {
+	} else if (["mlb", "outliers", "nhl", "atgs", "atgs2", "kotc", "nba", "pts", "analysis", "ncaab"].includes(PAGE)) {
 		fetchProps();
 		renderFilters();
 		initExcluded();

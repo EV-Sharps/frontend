780655334b3f5bb233ebce4341a525ea8f578f27
Zack Hecht <zhecht7@gmail.com>
2026-06-12 12:12:19 -0400

    free over

--- shared.js
+++ shared.js
@@ -87,7 +87,7 @@ const PAGE_SECTIONS = [
 		pages: [
 			{ label: "⚾ NCAA", value: "baseball_ncaa" },
 			{ label: "⚽ Soccer", value: "soccer" },
-			{ label: "🌍 World Cup", value: "cup" },
+			{ label: "🌍 World Cup", value: "cup", sharp: true },
 			{ label: "🥊 UFC", value: "ufc" },
 			{ label: "🗺️ Heat Map", value: "heatmap" },
 			{ label: "📋 Cheat Sheets", value: "cheat" },
